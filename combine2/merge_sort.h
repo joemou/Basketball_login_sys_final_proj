266ce9965bf0e9c60b61844abaf241cb82f1ac46
HowHao Chih <54176086+HaoGod666@users.noreply.github.com>
2023-06-10 15:54:51 +0800

    rename Mergesort

--- combine2/merge_sort.h
+++ combine2/merge_sort.h
@@ -2,13 +2,13 @@
 #define merge_sort_h
 #include "basic_func.h"
 
-//for mergesort
+// for mergesort
 /* Find mid and split */
 void split(node *first, node **a, node **b);
 
 /* merge the node */
 struct node *merge(node *a, node *b, int action);
 
-void mergesort(node **head, int action);
+void Mergesort(node **head, int action);
 
 #endif
